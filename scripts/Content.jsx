22484c1cc190b9b309a05c04a87ccadd6b0455ac
dhvani <dmm77@njit.edu>
2020-11-05 00:43:48 +0000

    created timeline component that will display posts

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { Socket } from './Socket';
 import StatusBar from './StatusBar';
+import Timeline from './Timeline'
 
 export function Content() {
    
     return (
         <div>
             <StatusBar />
+            
+            <Timeline />
         </div>
     );
 }
---
+++ scripts/Timeline.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Socket } from './Socket';
+
+export default function Timeline() {
+    
+    return (
+        <div>
+        </div>
+    );
+}
