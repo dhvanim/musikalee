2dfa7a66cc5dcdda66433872f963f7720f3b1931
dhvani <dmm77@njit.edu>
2020-11-12 16:27:26 +0000

    created function to get recc songs based on list of top artists

--- spotify_trending.py
+++ spotify_trending.py
@@ -57,3 +57,45 @@ def spotify_get_trending():
     
     return tracks_data['items']
     
+# returns recc songs
+def spotify_get_recommended(artists):
+    
+    access_token = spotify_get_access_token()
+    
+    header = { 'Authorization': 'Bearer {token}'.format(token=access_token) }
+
+    # get tracks API endpoint URL
+    rec_url = "https://api.spotify.com/v1/recommendations"
+    rec_body_params = { 'seed_artists': artists, 'limit':3 }
+    rec_response = requests.get(rec_url, headers=header, params=rec_body_params)
+
+    # if api response error
+    if rec_response.status_code != 200:
+        return None
+        
+    rec_data = rec_response.json()
+    
+    recs = []
+    for tracks in rec_data["tracks"]:
+        track = {}
+        
+        track["title"] = tracks["name"]
+        
+        artists_list = []
+        for artists in tracks["artists"]:
+            artists_list.append( artists["name"] )
+        track["artists"] = ", ".join(artists_list)
+        
+        recs.append(track)
+    
+    return recs
+        
+        
+artist_uris = ["06HL4z0CvFAxyc27GXpf02",
+                "74XFHRwlV6OrjEM0A2NCMF",
+                "5FxD8fkQZ6KcsSYupDVoSO",
+                "0AsThoR4KZSVktALiNcQwW",
+                "66CXWjxzNUsdJxJ2JdwvnR"]
+
+print (spotify_get_recommended( artist_uris ))
+    
