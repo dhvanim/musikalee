bcb5de2298e16fb545b66a16c2a53ddd6db866aa
EC2 Default User <ec2-user@ip-172-31-30-207.ec2.internal>
2020-11-12 21:01:36 +0000

    Added the framework for testing and added tests for spotifylogin

--- spotify_login.py
+++ spotify_login.py
@@ -8,8 +8,11 @@ def get_user(auth):
     Aquire A user object from Spotify using Auth Token
     """
     url = 'https://api.spotify.com/v1/me'
-    header = {"Authorization": "Bearer "+auth}
-    response=requests.get(url,headers=header)
+    try:
+        response=requests.get(url,headers={"Authorization": "Bearer "+auth})
+    except TypeError:
+        response=requests.get(url,headers='{"Authorization": "Bearer "+auth}')
+        
     unam=response.json()['display_name']
     try:
         pfp= response.json()['images'][0]['url']
@@ -26,13 +29,20 @@ def get_artists(auth):
     """
     Aquire the list of favorite artists
     """
-    headers = {
-   'Accept': 'application/json',
-   'Content-Type': 'application/json',
-    'Authorization': 'Bearer '+auth,
-    }
-    response = requests.get('https://api.spotify.com/v1/me/top/artists', headers=headers)
-    # print(response.json())
+    try:
+        response = requests.get(
+                            'https://api.spotify.com/v1/me/top/artists',
+                            headers={
+                                'Accept': 'application/json',
+                                'Content-Type': 'application/json',
+                                'Authorization': 'Bearer '+auth,
+                            }
+                            )
+                            
+    except TypeError:
+        response = requests.get(
+                            'https://api.spotify.com/v1/me/top/artists',
+                            headers="")
     uris=[]
     for item in response.json()['items']:
         uris.append(item['uri'])
