3a6c4ec4c4f9f81e8db567629cab87ae74b10d42
EC2 Default User <ec2-user@ip-172-31-41-174.ec2.internal>
2020-11-13 00:47:54 +0000

    getting info from spotify

--- scripts/SpotifyButton.jsx
+++ scripts/SpotifyButton.jsx
@@ -15,9 +15,9 @@ export default function SpotifyButton() {
       <SpotifyLogin
         clientId="d1252102702f4ae4bbadc59e907a87da"
         /* redirectUri={process.env.uri} */
-        redirectUri="https://63493edfefe54691b206383fc43f796b.vfs.cloud9.us-east-1.amazonaws.com/"
+        redirectUri="https://63493edfefe54691b206383fc43f796b.vfs.cloud9.us-east-1.amazonaws.com"
         onSuccess={onSuccess}
-        scope="user-read-email user-top-read user-follow-read"
+        scope="user-read-email user-top-read user-follow-read user-read-currently-playing"
         className="spotifybutton"
       />
     </div>
