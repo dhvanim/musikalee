30aa7e4eae618ddc464361ec8d73ebd0a4e3f423
dhvani <dmm77@njit.edu>
2020-12-03 04:39:01 +0000

    linted timeline

--- scripts/Timeline.jsx
+++ scripts/Timeline.jsx
@@ -1,126 +1,141 @@
 import * as React from 'react';
 import { Socket } from './Socket';
 
-import PostItem from "./PostItem";
+import PostItem from './PostItem';
 
 export default function Timeline() {
-    
-    const [posts, setPosts] = React.useState(() => {
-    const stickyValue = window.localStorage.getItem("posts");
+  const [posts, setPosts] = React.useState(() => {
+    const stickyValue = window.localStorage.getItem('posts');
     return stickyValue !== null
       ? JSON.parse(stickyValue)
       : [];
   });
-    
-    function getPosts() {
-        React.useEffect( () => {
-            let isMounted = true; 
-            Socket.on('emit posts channel', (posts) => { 
-                if(isMounted){ 
-                    setPosts( posts );
-                    
-                }});
-            window.localStorage.setItem("posts", JSON.stringify(posts));
-            return () => {
-                Socket.off('emit posts channel', true);
-                isMounted = false;
-            };
-        });
-    }
-    
-    
-    getPosts();
-    
-    function getNewPost() {
-        React.useEffect( () => {
-            let isMounted = true; 
-            Socket.on('emit new post channel', (new_post) => {
-                if(isMounted){
-                setPosts([new_post].concat(posts));
-                window.localStorage.setItem("posts", JSON.stringify(posts));
-                }
 
-            });
-            return () => {
-                Socket.off('emit new post channel',true);
-                isMounted = false;
-            };
-        });
-    }
+  function getPosts() {
+    React.useEffect(() => {
+      let isMounted = true;
+      Socket.on('emit posts channel', (data) => {
+        if (isMounted) {
+          setPosts(data);
+        }
+      });
+      window.localStorage.setItem('posts', JSON.stringify(posts));
+      return () => {
+        Socket.off('emit posts channel', true);
+        isMounted = false;
+      };
+    });
+  }
 
-    getNewPost();
+  getPosts();
 
-    function updateLikes() {
-        React.useEffect( () => {
-            let isMounted = true; // note this flag denote mount status to avoid "Can't perform a React state update on an unmounted component"
-            Socket.on('like post channel', (data) => {
-                if(isMounted){
-                    const num_likes = {num_likes: data.num_likes};
-                    const is_liked = {is_liked: data.is_liked};
-                    let Newposts = posts.map(el => (el.id === data.post_id ? Object.assign({}, el, num_likes, is_liked) : el));
-                    setPosts(
-                     Newposts
-                    );
-                    window.localStorage.setItem("posts", JSON.stringify(posts));                    
-                }
-            });
-            return () => {
-                Socket.off('like post channel', true);
-                isMounted = false;
-            };
-        });
-    }
-    
-    updateLikes();
-    
-    function updateComments() {
-        React.useEffect( () => {
-            let isMounted = true; // note this flag denote mount status
-            Socket.on('NEW COMMENT ON POST', (data) => {
-                if (isMounted){
-                    let Newposts = posts.map(el => (el.id === data.post_id ? Object.assign({}, el, {comments: [data.comment].concat(el.comments)}, {isCommentsOpen: true}) : el));
-                    setPosts(
-                     Newposts
-                    );
-                    window.localStorage.setItem("posts", JSON.stringify(posts));                    
-                }
+  function getNewPost() {
+    React.useEffect(() => {
+      let isMounted = true;
+      Socket.on('emit new post channel', (newPost) => {
+        if (isMounted) {
+          setPosts([newPost].concat(posts));
+          window.localStorage.setItem('posts', JSON.stringify(posts));
+        }
+      });
+      return () => {
+        Socket.off('emit new post channel', true);
+        isMounted = false;
+      };
+    });
+  }
 
-            });
-            return () => {
-                Socket.off('NEW COMMENT ON POST', true);
-                isMounted = false;
-            };
-        });
-    }
-    
-    updateComments();
-    
-    function getLocalStorage() {
-        React.useEffect( () => {
-            let isMounted = true; 
-            Socket.on('navigation change', (data) => {
-                if (isMounted){
-                    setPosts(window.localStorage.getItem("posts"));
-                }
-            });
-            return () => {
-                Socket.off('navigation change', true);
-                isMounted = false;
-            };
-        });
-    }
+  getNewPost();
 
-    getLocalStorage();
-    
-    return (
-        <div>
-        <ul className="timeline">
-            { 
-                posts.map( (post, index) => (
-                    <PostItem key={index} id={post.id} username={post.username} text={post.text} time={post.datetime} likes={post.num_likes} is_liked={post.is_liked} comments={post.comments} isCommentsOpen={post.isCommentsOpen} pfp={post.pfp} music={post.music} music_type={post.music_type}/>
-                ))    
+  function updateLikes() {
+    React.useEffect(() => {
+      // flags notes mount status to avoid
+      // "can't perform a React state update on an unmounted component"
+      let isMounted = true;
+      Socket.on('like post channel', (data) => {
+        if (isMounted) {
+          const numLikes = { num_likes: data.num_likes };
+          const isLiked = { is_liked: data.is_liked };
+          const Newposts = posts.map((el) => (
+            el.id === data.post_id ? ({ ...el, ...numLikes, ...isLiked }) : el));
+          setPosts(
+            Newposts,
+          );
+          window.localStorage.setItem('posts', JSON.stringify(posts));
+        }
+      });
+      return () => {
+        Socket.off('like post channel', true);
+        isMounted = false;
+      };
+    });
+  }
+
+  updateLikes();
+
+  function updateComments() {
+    React.useEffect(() => {
+      let isMounted = true; // note this flag denote mount status
+      Socket.on('NEW COMMENT ON POST', (data) => {
+        if (isMounted) {
+          const Newposts = posts.map((el) => (
+            el.id === data.post_id ? (
+              { ...el, comments: [data.comment].concat(el.comments), isCommentsOpen: true }
+            ) : el));
+          setPosts(
+            Newposts,
+          );
+          window.localStorage.setItem('posts', JSON.stringify(posts));
+        }
+      });
+      return () => {
+        Socket.off('NEW COMMENT ON POST', true);
+        isMounted = false;
+      };
+    });
+  }
+
+  updateComments();
+
+  function getLocalStorage() {
+    React.useEffect(() => {
+      let isMounted = true;
+      Socket.on('navigation change', (data) => {
+        if (isMounted) {
+          setPosts(window.localStorage.getItem('posts'));
+        }
+      });
+      return () => {
+        Socket.off('navigation change', true);
+        isMounted = false;
+      };
+    });
+  }
+
+  getLocalStorage();
+
+  return (
+    <div>
+      <ul className="timeline">
+        {
+                posts.map((post, index) => (
+                  <PostItem
+                    key={index}
+                    id={post.id}
+                    username={post.username}
+                    text={post.text}
+                    time={post.datetime}
+                    likes={post.num_likes}
+                    is_liked={post.is_liked}
+                    comments={post.comments}
+                    isCommentsOpen={post.isCommentsOpen}
+                    pfp={post.pfp}
+                    music={post.music}
+                    music_type={post.music_type}
+                  />
+                ))
             }
-        </ul>
-        </div>
-    );
+      </ul>
+    </div>
+  );
 }
